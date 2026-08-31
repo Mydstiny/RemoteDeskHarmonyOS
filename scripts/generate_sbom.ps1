58158a509f6368c3ff72537d4b237cd830308570
Mydstiny <181178314+Mydstiny@users.noreply.github.com>
2026-08-31 12:16:45 +0800

    feat(release): prepare 1.1.5 update swiper

--- scripts/generate_sbom.ps1
+++ scripts/generate_sbom.ps1
@@ -38,7 +38,7 @@ $packages = [System.Collections.Generic.List[object]]::new()
 $packages.Add([ordered]@{
   name = 'RemoteDeskHarmonyOS'
   SPDXID = 'SPDXRef-Package-RemoteDeskHarmonyOS'
-  versionInfo = '1.1.4'
+  versionInfo = '1.1.5'
   downloadLocation = 'https://github.com/Mydstiny/RemoteDeskHarmonyOS'
   filesAnalyzed = $false
   licenseConcluded = 'AGPL-3.0-or-later'
